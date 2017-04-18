715230da5d8c1308470a5922f4ef34336482cd28
tracylm <tracy.murray@yahoo.com>
2017-04-18 12:55:31 -0400

    Set up a page for each object.

--- collection/views.py
+++ collection/views.py
@@ -7,3 +7,9 @@ def index(request):
     return render(request, 'index.html', {
         'things': things
     })
+
+def thing_detail(request, slug):
+    thing = Thing.objects.get(slug=slug)
+    return render(request, 'things/thing_detail.html', {
+        'thing': thing,
+    })
