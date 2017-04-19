49ddeb263c5052d5e0a96032030807dc5bca2fb3
tracylm <tracy.murray@yahoo.com>
2017-04-19 12:12:26 -0400

    Added forms to update info for each object on the site.

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -22,10 +22,12 @@ urlpatterns = [
     url(r'^$', views.index, name='home'),
     url(r'^about/$',
         TemplateView.as_view(template_name='about.html'), name='about'),
-    url(r'contact/$',
+    url(r'^contact/$',
         TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^things/(?P<slug>[-\w]+)/$', views.thing_detail,
         name='thing_detail'),
+    url(r'^things/(?P<slug>[-\w]+)/edit/$',
+        views.edit_thing,
+        name='edit_thing'),
     url(r'^admin/', admin.site.urls),
-
 ]
