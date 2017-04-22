83518491a5c6e4742190adb5393163299561fbdb
tracylm <tracy.murray@yahoo.com>
2017-04-22 16:23:22 -0400

    Added 500 and 400 pages, requirements.txt and began testing.

--- hellowebapp/settings.py
+++ hellowebapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4)dbah$0(#t)rx_ma16m8nb)ri^w-io&udd8_ixism^iu^6s(y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
